d238a8b127eadab3f3bc345439028cdef1d6d6d4
Amidou Bibata <a.bibata@kaayu.ca>
2018-07-09 12:15:39 -0400

    utilisation des fonctions callbacks

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-var request = require('request');
+//const request = require('request');
+const yargs =require('yargs');
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather.js');
+const argv = yargs
+ .options({
+	a: {
+      demand: true,     
+      alias: 'address',  
+      describe: 'Address to fetch weather for',   
+      string: true
+    }
+  })
+    .help()
+    .alias('help', 'h')  
+    .argv;
 
-request({
-    url:'http://maps.googleapis.com/maps/api/geocode/json?address=1220%20rue%20gohier%20saint-laurent',
-    json: true
-    }, (error, response, body) =>{
-        console.log(body); 
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(results.address);
+    weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {//appel d'une fonction ds une fct   
+      if (errorMessage) {
+        console.log(errorMessage);
+      } else {
+        console.log(`It's currently ${weatherResults.temperature}. 
+        It feels like ${weatherResults.apparentTemperature}.`);
+      }
+    });
+  }
 });
+
+
+//weather.getWeather(); appel de la fonction sans param
+      
+
